cdcb1d382b4fd77ac952d5833e5621504b200012
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-11-30 05:42:38 +0545

    job card has also been saved success fully.

--- RoyalBikeSmith/user/views.py
+++ RoyalBikeSmith/user/views.py
@@ -7,6 +7,7 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from django.urls import reverse
 from .forms import CustomerForm, JobCardForm
+from .models import JobCard
 # Create your views here.
 
 @login_required
@@ -23,8 +24,13 @@ def create_job_card(request):
         customer_form = CustomerForm(data=request.POST)
         job_card_form = JobCardForm(data=request.POST)
 
-        print(customer_form.is_valid())
-        print(job_card_form.is_valid())
+        get_invoice_no = request.POST['invoice_no']
+        get_part_number = request.POST.getlist('part_number')
+        get_part_name = request.POST.getlist('part_name')
+        get_quantity = request.POST.getlist('quantity')
+        get_price = request.POST.getlist('price')
+        get_total = request.POST.getlist('total')
+        get_grand_total = request.POST['grand_total']
 
         if customer_form.is_valid() and job_card_form.is_valid():
         
@@ -32,9 +38,12 @@ def create_job_card(request):
             customer_save.user_id = request.user.id 
             customer_save.save()
             
-            job_save = job_card_form.save(commit=False)
-            job_save.customer_id = customer_save.id
-            job_save.save()
+            # job_save = job_card_form.save(commit=False)
+            # job_save.customer_id = customer_save.id
+            # job_save.save()
+
+            for a,b,c,d,e in zip(get_part_number,get_part_name, get_quantity,get_price,get_total):
+                JobCard.objects.create(customer_id=customer_save.id, invoice_no=get_invoice_no, part_number=a,part_name=b, quantity=c, price=d, total=e, grand_total=get_grand_total)
 
     else:
         customer_form = CustomerForm()
