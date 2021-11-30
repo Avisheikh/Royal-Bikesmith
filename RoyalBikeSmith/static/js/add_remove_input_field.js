b1c7128ddd5a45000824770efa256628ba4c8a33
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-11-30 21:54:16 +0545

    remove tr created successfully.

--- RoyalBikeSmith/static/js/add_remove_input_field.js
+++ RoyalBikeSmith/static/js/add_remove_input_field.js
@@ -11,18 +11,16 @@ $(document).ready(function(){
 
 		if(counter < max_fields){
 			counter++;
-			$(wrapper).append('<tr><td class="col-md-2"><input type="text" class="form-control" name="part_number"></td><td class="col-md-2"><input type="text" class="form-control" name="part_name"></td><td class="col-md-2"><input type="number"  class="form-control" name="quantity"></td><td class="col-md-2"><input type="number" pattern="^\d*(\.\d{0,2})?$" class="form-control" name="price"></td><td class="col-md-2"><input type="number" class="form-control" pattern="^\d*(\.\d{0,2})?$" name="total"></td><td class="col-sm-1"><input type="checkbox" class="form-control" name="record"></td></tr>');
+			$(wrapper).append('<tr><td class="col-md-2"><input type="text" class="form-control" name="part_number"></td><td class="col-md-2"><input type="text" class="form-control" name="part_name"></td><td class="col-md-2"><input type="number"  class="form-control" name="quantity"></td><td class="col-md-2"><input type="number" pattern="^\d*(\.\d{0,2})?$" class="form-control" name="price"></td><td class="col-md-2"><input type="number" class="form-control" pattern="^\d*(\.\d{0,2})?$" name="total"></td><td class="col-sm-1 text-center"><button class="remove btn btn-danger remove" type="button">Remove</button></td></tr>');
 		}
 
 	});
-	
-	// Find and remove selected table rows
-	$(".delete_field_button").click(function(){
-		$("table tbody").find('input[name="record"]').each(function(){
-			if($(this).is(":checked")){
-				$(this).parents("tr").remove();
-			}
-		});
+
+	// jquery button click event to remove a row
+	$(document).on('click', 'button.remove', function(){
+		$(this).closest('tr').remove();
+		counter--;
+		return false;
 	});
 });
 
