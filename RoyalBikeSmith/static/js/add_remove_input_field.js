349eba5797c0d7f20f659ac01ff41bc08ec3881a
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-11-30 06:38:50 +0545

    aba check box implement garnu parcha

--- RoyalBikeSmith/static/js/add_remove_input_field.js
+++ RoyalBikeSmith/static/js/add_remove_input_field.js
@@ -1,6 +1,6 @@
 // Add multiple input field for extra attributes and value
 $(document).ready(function(){
-	var max_fields = 10; // maximum input boxes allowed
+	var max_fields = 100; // maximum input boxes allowed
 	var wrapper = $(".input_fields_wrap");//Fields wrapper
 	var add_button = $(".add_field_button"); // Add button Id
    
@@ -11,21 +11,18 @@ $(document).ready(function(){
 
 		if(counter < max_fields){
 			counter++;
-			$(wrapper).append('<tr><td class="col-md-2"><input type="text" class="form-control" name="part_number"></td><td class="col-md-2"><input type="text" class="form-control" name="part_name"></td><td class="col-md-2"><input type="number"  class="form-control" name="quantity"></td><td class="col-md-2"><input type="number" pattern="^\d*(\.\d{0,2})?$" class="form-control" name="price"></td><td class="col-md-2"><input type="number" class="form-control" pattern="^\d*(\.\d{0,2})?$" name="total"></td></tr>');
+			$(wrapper).append('<tr><td class="col-md-2"><input type="text" class="form-control" name="part_number"></td><td class="col-md-2"><input type="text" class="form-control" name="part_name"></td><td class="col-md-2"><input type="number"  class="form-control" name="quantity"></td><td class="col-md-2"><input type="number" pattern="^\d*(\.\d{0,2})?$" class="form-control" name="price"></td><td class="col-md-2"><input type="number" class="form-control" pattern="^\d*(\.\d{0,2})?$" name="total"></td><td class="col-sm-1"><input type="checkbox" class="form-control" name="record"></td></tr>');
 		}
 
 	});
-	$(wrapper).on("click",".remove_field", function(e){ //user click on remove text
-		e.preventDefault(); 
-		$(this).parent('div').remove(); 
-		counter--;
-	})
+	
+	// Find and remove selected table rows
+	$(".delete_field_button").click(function(){
+		$("table tbody").find('input[name="record"]').each(function(){
+			if($(this).is(":checked")){
+				$(this).parents("tr").remove();
+			}
+		});
+	});
 });
 
-// Add multiple image limit in chossen image
-$("#limitImg").on("change", function(){
-	if($("#limitImg")[0].files.length > 2){
-		alert("You func");
-		$("#limitImg").val('');
-	}
-});
