fb45a967aea5b100fff58a4a4d72833de610bf1e
Abishek Bhandari <abishekbhandari33@gmail.com>
2021-12-03 07:55:14 +0545

    updated models

--- RoyalBikeSmith/user/models.py
+++ RoyalBikeSmith/user/models.py
@@ -18,6 +18,7 @@ class Customer(models.Model):
     work_assigned = models.CharField(max_length=150)
     maintenance_type = models.CharField(max_length=100)
     created = models.DateTimeField(auto_now_add=True)
+    grand_total = models.DecimalField(max_digits=10, decimal_places=2)
 
     def __str__(self):
         return self.first_name
@@ -29,7 +30,7 @@ class JobCard(models.Model):
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
     total = models.DecimalField(max_digits=10, decimal_places=2)
-    grand_total = models.DecimalField(max_digits=10, decimal_places=2)
+    
 
     def __str__(self):
         return self.part_name
